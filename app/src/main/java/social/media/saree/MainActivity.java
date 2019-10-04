a6b10ccb5e00a2148d9591b4b5993b91f54f3bd5
Mufasa <fashioncstar2019@outlook.com>
2019-10-04 13:24:06 +0700

    test commit
    this is for test

--- app/src/main/java/social/media/saree/MainActivity.java
+++ app/src/main/java/social/media/saree/MainActivity.java
@@ -1,24 +1,85 @@
 package social.media.saree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.microedition.khronos.opengles.GL;
+
+import social.media.saree.member.Member;
 import social.media.saree.product.upload_product;
+import social.media.saree.saree.saree;
+import social.media.saree.saree.saree_view;
+import social.media.saree.util.Global;
 
 public class MainActivity extends AppCompatActivity {
     public static MainActivity myself;
+    LinearLayout Fashion_saree, BestSeller_saree, New_Saree, Upload_product, Order_manager, adminPanel;
+    ImageView Image_area_a, Image_area_b;
+    String TAG = "MainActivity";
+    private ArrayList<Member> array_all_members = new ArrayList<Member>();
+    private ArrayList<saree> array_all_sarees = new ArrayList<saree>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button upload = (Button) findViewById(R.id.btn_upload_product);
-        upload.setOnClickListener(new View.OnClickListener() {
+        Image_area_a = (ImageView)findViewById(R.id.image_area_a);
+        Image_area_b = (ImageView)findViewById(R.id.image_area_b);
+        Fashion_saree = (LinearLayout)findViewById(R.id.fashion_sarees);
+        BestSeller_saree = (LinearLayout)findViewById(R.id.best_seller);
+        New_Saree = (LinearLayout)findViewById(R.id.new_saree);
+        Upload_product = (LinearLayout)findViewById(R.id.btn_upload_product);
+        Order_manager = (LinearLayout)findViewById(R.id.order_manager);
+        adminPanel = (LinearLayout)findViewById(R.id.admin_panel);
+
+        final Intent intent = new Intent(MainActivity.this, saree_view.class);
+        Fashion_saree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Image_area_a.setBackgroundResource(R.drawable.select_fashion);
+                intent.putExtra("Style","Fashion");
+                startActivity(intent);
+            }
+        });
+        BestSeller_saree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Image_area_a.setBackgroundResource(R.drawable.select_best_seller);
+                intent.putExtra("Style","Best Seller");
+                startActivity(intent);
+            }
+        });
+        New_Saree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Image_area_a.setBackgroundResource(R.drawable.select_new);
+                intent.putExtra("Style","New");
+                startActivity(intent);
+            }
+        });
+        Upload_product.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, upload_product.class);
@@ -26,6 +87,180 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         myself = this;
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        FirebaseApp.initializeApp(this);
+        try {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+        catch (Exception e){
+            Log.e(TAG, e.toString());
+        }
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Member");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is:" + value);
+                Global.array_all_members.clear();
+                if (dataSnapshot.exists()){
+                    HashMap<String, Object> dataMap = (HashMap<String, Object>) dataSnapshot.getValue();
+//                    Member Member = dataSnapshot.getValue(Member.class);
+//                    Log.d(TAG, "User name: " + Member.getName() + ", email " + Member.getEmail());
+                    for (String key : dataMap.keySet()){
+
+                        Object data = dataMap.get(key);
+
+                        try{
+                            HashMap<String, Object> userData = (HashMap<String, Object>) data;
+                            String userName = userData.get("Name").toString();
+                            String userEmail = userData.get("Personal Email").toString();
+                            String userGender = userData.get("Gender").toString();
+                            String password = userData.get("Password").toString();
+                            String user_designation = userData.get("Designation").toString();
+                            String user_official_email = userData.get("Official Email").toString();
+                            String user_official_number = userData.get("Official Number").toString();
+                            String user_personal_number = userData.get("Personal Number").toString();
+                            String base64photo = "";
+                            String birthday = null;
+                            String address = null;
+                            String location = null;
+                            try {
+                                base64photo = userData.get("Photo").toString();
+                            }
+                            catch (Exception E){
+                                Log.e(TAG,E.toString());
+                            }
+                            try {
+                                birthday = userData.get("Birthday").toString();
+                            }
+                            catch (Exception E){
+                                Log.e(TAG,E.toString());
+                            }
+                            try {
+                                address = userData.get("Address").toString();
+                            }
+                            catch (Exception E){
+                                Log.e(TAG,E.toString());
+                            }
+                            try {
+                                location = userData.get("Location").toString();
+                            }
+                            catch (Exception e)
+                            {
+                                Log.e(TAG, "user profile is not complited");
+                                if (userEmail.equals(Global.current_user_email))
+                                    Toast.makeText(MainActivity.this,"You didn't complete profile. please complete the profile",Toast.LENGTH_LONG).show();
+                            }
+
+                            if(userEmail.equals(Global.current_user_email)) {
+                                Global.current_user_name = userName;
+                                Global.current_user_index = array_all_members.size();
+                                try {
+                                    Global.current_user_photo = base64photo;
+                                }
+                                catch (Exception e){
+                                    Log.e(TAG,e.toString());
+                                }
+                                try {
+                                    String permission = userData.get("Permission").toString();
+                                    if (permission.equals("admin")) {
+                                        Global.is_admin = true;
+                                        adminPanel.setVisibility(View.VISIBLE);
+                                    }
+
+                                }
+                                catch (Exception e){
+                                    Log.e(TAG, "No permission");
+
+                                }
+                            }
+                            array_all_members.add(new Member(userName, userEmail, userGender, base64photo, birthday, address, location, password, user_designation, user_official_email, user_official_number, user_personal_number));
+                        }catch (Exception cce){
+                            Log.e(TAG, cce.toString());
+                        }
+
+                    }
+                    Global.array_all_members = array_all_members;
+                    progressDialog.dismiss();
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG,"Failed to rad value ", databaseError.toException());
+                progressDialog.dismiss();
+            }
+        });
+
+        myRef = database.getReference("Product/Saree");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Global.array_all_sarees.clear();
+                Global.array_Fashion_sarees.clear();
+                Global.array_BestSeller_sarees.clear();
+                Global.array_New_sarees.clear();
+
+                if (dataSnapshot.exists()){
+                    HashMap<String, Object> dataMap = (HashMap<String, Object>) dataSnapshot.getValue();
+
+                    for (String key : dataMap.keySet()){
+
+                        Object data = dataMap.get(key);
+
+                        try{
+                            HashMap<String, Object> userData = (HashMap<String, Object>) data;
+                            String saree_Id = userData.get("Id").toString();
+                            String saree_Name = userData.get("Name").toString();
+                            String saree_Label = userData.get("Label").toString();
+                            String saree_Price = userData.get("Price").toString();
+                            String saree_Material = userData.get("Material").toString();
+                            String saree_Length = userData.get("Length").toString();
+                            String saree_Occations = userData.get("Occations").toString();
+                            String saree_Color = userData.get("Color").toString();
+                            String saree_Rating = userData.get("Rating").toString();
+                            String saree_Description = userData.get("Description").toString();
+                            String saree_Style = userData.get("Style").toString();
+                            String base64photo = "";
+                            try {
+                                base64photo = userData.get("Photo").toString();
+                            }
+                            catch (Exception E){
+                                Log.e(TAG,E.toString());
+                            }
+                            saree new_saree = new saree(saree_Id, saree_Name, saree_Label, saree_Price, saree_Material, saree_Length, saree_Occations, saree_Color, saree_Rating, saree_Description, saree_Style,base64photo, null, null, null ) ;
+                            array_all_sarees.add(new_saree);
+                            if (saree_Style.equals("Fashion"))
+                                Global.array_Fashion_sarees.add(new_saree);
+                            else if (saree_Style.equals("Best Seller"))
+                                Global.array_BestSeller_sarees.add(new_saree);
+                            else if (saree_Style.equals("New"))
+                                Global.array_New_sarees.add(new_saree);
+                        }catch (Exception cce){
+                            Log.e(TAG, cce.toString());
+                        }
+
+                    }
+                    Global.array_all_sarees = array_all_sarees;
+                    progressDialog.dismiss();
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG,"Failed to rad value ", databaseError.toException());
+                progressDialog.dismiss();
+            }
+        });
     }
 
     public static MainActivity getInstance(){
--- app/src/main/java/social/media/saree/product/upload_product.java
+++ app/src/main/java/social/media/saree/product/upload_product.java
@@ -1,10 +1,35 @@
 package social.media.saree.product;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 import social.media.saree.R;
 
@@ -16,21 +41,201 @@ public class upload_product extends AppCompatActivity {
     EditText saree_Length;
     EditText saree_Occations;
     EditText saree_Color;
+    EditText saree_Material;
     EditText saree_custom_a;
     EditText saree_custom_b;
     EditText saree_custom_c;
+    Spinner saree_style;
+    Button upload_product;
+    String saree_id;
+    private Calendar calendar;
+    private int year, month, day;
+
+    public static final int PICK_IMAGE = 1;
+    public Bitmap bitmap = null;
+    public Uri filePath ;
+
+    private String TAG = "Upload_product";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_product);
 
+        saree_photo = (ImageView)findViewById(R.id.image_saree);
         saree_Name = (EditText)findViewById(R.id.saree_name);
         saree_Label = (EditText)findViewById(R.id.saree_label);
         saree_Price = (EditText)findViewById(R.id.saree_price);
         saree_Length = (EditText)findViewById(R.id.saree_length);
         saree_Occations = (EditText)findViewById(R.id.saree_occasions);
         saree_Color = (EditText)findViewById(R.id.saree_color);
+        upload_product = (Button)findViewById(R.id.btn_upload);
+        saree_Material = (EditText)findViewById(R.id.saree_material);
+        saree_style = (Spinner)findViewById(R.id.saree_style);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        saree_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+
+        upload_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate()) {
+                    Date currentTime = Calendar.getInstance().getTime();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
+                    String str = sdf.format(currentTime);
+                    String sub_id = String.valueOf(currentTime.getHours())+String.valueOf(currentTime.getMinutes());
+                    saree_id =  String.valueOf(year)  + String.valueOf(month + 1)  +String.valueOf(day)  + str;
+                    upload_product();
+                }
+            }
+        });
+
 
 
     }
+
+    private boolean validate() {
+        if (checkColor() & checkName() & checkLabel() & checkOccations() & checkPrice())
+            return true;
+        else
+        return false;
+    }
+
+    public boolean checkName() {
+
+        if ( !saree_Name.getText().equals("")){
+            return true;
+        } else {
+            saree_Name.setError("Input Saree Name");
+            return false;
+        }
+    }
+    public boolean checkLabel() {
+
+        if ( !saree_Label.getText().equals("")){
+            return true;
+        } else {
+            saree_Label.setError("Input Saree Name");
+            return false;
+        }
+    }
+    public boolean checkPrice() {
+
+        if ( !saree_Price.getText().equals("")){
+            return true;
+        } else {
+            saree_Price.setError("Input Saree Name");
+            return false;
+        }
+    }
+    public boolean checkOccations() {
+
+        if ( !saree_Occations.getText().equals("")){
+            return true;
+        } else {
+            saree_Occations.setError("Input Saree Name");
+            return false;
+        }
+    }
+    public boolean checkColor() {
+
+        if ( !saree_Color.getText().equals("")){
+            return true;
+        } else {
+            saree_Color.setError("Input Saree Name");
+            return false;
+        }
+    }
+
+
+
+    private void upload_product(){
+        FirebaseApp.initializeApp(this);
+        String id = "Product/Saree/" + saree_id;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(id+"/Name");
+        myRef.setValue(saree_Name.getText().toString());
+        myRef = database.getReference(id+"/Label");
+        myRef.setValue(saree_Label.getText().toString());
+        myRef = database.getReference(id+"/Price");
+        myRef.setValue(saree_Price.getText().toString());
+        myRef = database.getReference(id+"/Length");
+        myRef.setValue(saree_Length.getText().toString());
+        myRef = database.getReference(id+"/Occations");
+        myRef.setValue(saree_Occations.getText().toString());
+        myRef = database.getReference(id+"/Color");
+        myRef.setValue(saree_Color.getText().toString());
+        myRef = database.getReference(id+"/Id");
+        myRef.setValue(saree_id);
+        myRef = database.getReference(id+"/Material");
+        myRef.setValue(saree_Material.getText().toString());
+        myRef = database.getReference(id+"/Style");
+        myRef.setValue(saree_style.getSelectedItem().toString());
+        myRef = database.getReference(id+"/Rating");
+        myRef.setValue("0");
+        if (bitmap!=null) {
+            myRef = database.getReference(id + "/Photo");
+            myRef.setValue(getBase64String(bitmap));
+        }
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is:" + value);
+                Toast.makeText(upload_product.this,"New Saree uploaded successully",Toast.LENGTH_LONG).show();
+                finish();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG,"Failed to rad value ", databaseError.toException());
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                saree_photo.setImageBitmap(bitmap);
+                filePath = data.getData();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * encode
+     * @param bitmap
+     * @return String
+     */
+    private String getBase64String(Bitmap bitmap) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 40, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
 }
